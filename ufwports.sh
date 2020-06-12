c6a7727904a947de468e01391a977e94d5d1408b
DeckerSU <deckersu@protonmail.com>
2020-06-12 22:27:47 +0300

    README.md update and scripts fixes

--- ufwports.sh
+++ ufwports.sh
@@ -16,7 +16,7 @@ do
    rpcport=$((rpcport+1))
    zmqport=$((zmqport+1))
    webport=$((webport+1))
-   daemon_getinfo=$(komodo/src/komodo-cli -ac_name=$i getinfo)
+   daemon_getinfo=$(komodo/src/komodo-cli -ac_name=$i getinfo 2>/dev/null)
    daemon_name=$(echo $daemon_getinfo | jq .name)
    daemon_name=$(echo $daemon_name | tr -d '"')
    daemon_rpcport=$(echo $daemon_getinfo | jq .rpcport)
